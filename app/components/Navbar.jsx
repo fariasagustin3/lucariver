2318f6178785d1d76bb61a6a8ce4bdd207d086be
fariasagustin3 <fariasagustin3@gmail.com>
2023-12-12 10:05:37 -0300

    feat(components) navbar added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,25 +4,28 @@ import { roboto } from '../fonts';
 
 const Navbar = () => {
   return (
-    <div className='w-screen h-20 py-5 px-5 flex items-center bg-[#690C36]'>
-      <ul className={`${roboto.className} flex flex-row items-center justify-center w-screen gap-7 text-lg text-white hover:underline`}>
+    <div className={`${roboto.className} w-screen h-20 py-5 sm:px-10 px-5 flex items-center justify-between gap-5 sm:justify-between bg-[#690C36] text-white text-lg`}>
+      <div className='sm:hidden'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-8 h-8">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+      </div>
+      <div className='flex items-center gap-3'>
         <Link href="#">
-          <li>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-8 h-8">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-          </li>
+          <span className='hover:underline cursor-pointer'>Home</span>
         </Link>
         <Link href="#">
-          <li>Home</li>
+          <span className='hover:underline cursor-pointer'>Über micht</span>
         </Link>
         <Link href="#">
-          <li>Über micht</li>
+          <span className='hover:underline cursor-pointer'>Preise/Kontakt</span>
         </Link>
-        <Link href="#">
-          <li>Preise/Kontakt</li>
-        </Link>
-      </ul>
+      </div>
+      <div className='hidden sm:flex gap-5'>
+        <span>Tel: 0159 015 00 311</span>
+        <span>Mail: a-z@mailbox.org</span>
+        <span>Sparrstr. 19 Berlin Wedding</span>
+      </div>
     </div>
   );
 }
